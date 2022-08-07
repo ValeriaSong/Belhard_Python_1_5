f6ff8b4e80aebb8ecfcbb0cd0c01cd65584d0914
Lera <newletterone@mail.ru>
2022-08-07 18:43:42 +0300

    easy cycles done

--- tasks/easy/cycles/odd.py
+++ tasks/easy/cycles/odd.py
@@ -20,6 +20,10 @@ some_list1 = []
 def odd_in_list(some_list: list) -> list:
     odd_list = []
     # TODO Написать свой код здесь
+    for element in some_list:
+        if not type(element) == int or element % 2 != 0:
+            continue
+        odd_list.append(element)
     return odd_list
 
 
